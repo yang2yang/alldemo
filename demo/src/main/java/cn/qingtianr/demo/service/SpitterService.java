0b4235cdf25504672b5701723ab249dcd339e38c
jack <qingliding@gmail.com>
2016-11-20 16:30:27 +0800

    使用xml可以成功注入

--- demo/src/main/java/cn/qingtianr/demo/service/SpitterService.java
+++ demo/src/main/java/cn/qingtianr/demo/service/SpitterService.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
  * Hessian的接口，定义在这个包中其实不是很好
 **  author:jack 2016年11月2016/11/19日
 */
-@Service
+//@Service
 public interface SpitterService {
     /**
      * 返回一个字符串
--- demo/src/main/java/cn/qingtianr/demo/springconfig/SpringConfig.java
+++ demo/src/main/java/cn/qingtianr/demo/springconfig/SpringConfig.java
@@ -9,14 +9,14 @@ import org.springframework.remoting.caucho.HessianProxyFactoryBean;
 /**
 **  author:jack 2016年11月2016/11/19日
 */
-@Configuration
-@ComponentScan("cn.qingtianr.demo")
+//@Configuration
+//@ComponentScan("cn.qingtianr.demo")
 public class SpringConfig {
 
     @Bean
     public SpitterService spitterService(){
         HessianProxyFactoryBean proxy = new HessianProxyFactoryBean();
-        proxy.setServiceUrl("http:localhost:8080/spitter/spitter/spitter.service");
+        proxy.setServiceUrl("http://localhost:8080/spitter.service");
         proxy.setServiceInterface(SpitterService.class);
         return (SpitterService) proxy.getObject();
     }
