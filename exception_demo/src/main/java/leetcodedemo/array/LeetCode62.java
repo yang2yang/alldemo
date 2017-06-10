c6e7d9f509d34bf0b117bc83f6249e2c12d0abb6
jack <qingliding@gmail.com>
2017-06-10 21:37:32 +0800

    LeetCode62

---
+++ exception_demo/src/main/java/leetcodedemo/array/LeetCode62.java
@@ -0,0 +1,29 @@
+package leetcodedemo.array;
+
+/**
+ * Created by jack-pc on 2017/6/10.
+ */
+public class LeetCode62 {
+    public static void main(String[] args) {
+
+    }
+
+    public int uniquePaths(int m, int n) {
+        int[][] nums = new int[m][n];
+        for (int i = 0; i < n; i++) {
+            nums[0][i] = 1;
+        }
+
+        for (int i = 0; i < m; i++) {
+            nums[i][0] = 1;
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                nums[i][j] = nums[i - 1][j] + nums[i][j - 1];
+            }
+        }
+
+        return nums[m - 1][n - 1];
+    }
+}
