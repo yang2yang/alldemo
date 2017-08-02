82c138fc8c8746abd9b38801bb4ffe56f691a8c5
丁庆立 <qingliding@gmail.com>
2017-08-02 17:03:55 +0800

    hdfs的java api

---
+++ exception_demo/src/main/java/hdfsdemo/hdfsAPIDemo.java
@@ -0,0 +1,40 @@
+package hdfsdemo;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileStatus;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+
+import java.net.URI;
+
+/**
+ * Created by jack on 2017/8/2.
+ */
+public class hdfsAPIDemo {
+
+    public static void main(String[] args) throws Exception {
+        putFile();
+    }
+
+    /**
+     * 将本地源文件上传到hdfs的目标文件中
+     * @throws Exception
+     */
+    public static void putFile() throws Exception{
+        FileSystem fs = FileSystem.get(new URI("hdfs://localhost:9001"),new Configuration());
+        fs.copyFromLocalFile(new Path("/temptemp.txt"),new Path("/temptemp.txt"));
+    }
+
+    /**
+     * 显示特定hdfs的目录下面的文件
+     * @throws Exception
+     */
+    public static void listFiles() throws Exception{
+        FileSystem fs = FileSystem.get(new URI("hdfs://localhost:9001"),new Configuration());
+        FileStatus[] list = fs.listStatus(new Path("/"));
+        for (FileStatus f:list) {
+            System.out.println(f.getPath());
+        }
+    }
+
+}
