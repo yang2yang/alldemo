d7803248eeb6f9a7c390ef784eeb5581619d53e3
丁庆立 <qingliding@gmail.com>
2017-09-12 09:57:51 +0800

    测试nio失败

---
+++ exception_demo/src/main/java/nioselectordemo/demo1/SelectorClient3.java
@@ -0,0 +1,30 @@
+package nioselectordemo.demo1;
+
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+
+/**
+ * Created by jack on 2017/9/8.
+ */
+public class SelectorClient3 {
+    public static void main(String[] args) {
+        try {
+            SocketChannel socketChannel = SocketChannel.open();
+            socketChannel.connect(new InetSocketAddress("127.0.0.1", 5000));
+            ByteBuffer writeBuffer = ByteBuffer.allocate(32);
+            ByteBuffer readBuffer = ByteBuffer.allocate(32);
+            writeBuffer.put("hello3".getBytes());
+            writeBuffer.flip(); // make buffer ready for reading
+            while (true) {
+                writeBuffer.rewind(); // sets the position back to 0
+                socketChannel.write(writeBuffer); // hello
+                readBuffer.clear(); // make buffer ready for writing
+                socketChannel.read(readBuffer); // recieved
+            }
+        } catch (Exception e) {
+
+        }
+
+    }
+}
