8abb4eeb0c98dc68147c389d73535266b799f16a
qingli.ding <qingliding@gmail.com>
2017-11-02 15:27:55 +0800

    看看mvc开启requestmapping之类注解的作用，必须要使用mvc:driven

--- demo/src/main/java/cn/qingtianr/demo/controller/BeforeController.java
+++ demo/src/main/java/cn/qingtianr/demo/controller/BeforeController.java
@@ -4,12 +4,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.annotation.PostConstruct;
+
 /**
  * Created by ding on 2017/3/31.
  */
 @Controller(value = "before")
 @RequestMapping(value="/before")
 public class BeforeController {
+
+    @PostConstruct
+    public void init(){
+        System.out.println("It is beforeController init method!");
+    }
+
 
     @ResponseBody
     @RequestMapping(value="/index")
---
+++ exception_demo/src/main/java/testinterface/Test.java
@@ -0,0 +1,10 @@
+package testinterface;
+
+/**
+ * Created by jack on 2017/10/25.
+ */
+public class Test {
+
+    public static void main(String[] args) {
+    }
+}
